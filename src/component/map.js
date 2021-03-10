65e93b1af7397556ff857663d76062e04371e97d
Trần Đức Nam <gionhe00co@gmail.com>
2021-03-10 15:38:36 +0700

    Demo map with tooltip

---
+++ src/component/map.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import React, { Component } from 'react';
+import ReactMapGL from 'react-map-gl';
+
+class map extends Component {
+    render() {
+        return (
+            <div>
+                 
+            </div>
+        );
+    }
+}
+
+export default map;
